9a4ad5d2418baab078ff7998adcfb04ae242ebef
Jair-F <78746086+Jair-F@users.noreply.github.com>
2021-04-08 20:25:21 +0300

    Hat vorher nicht funktioniert - jetzt aber

--- C++/Problem 14.cpp
+++ C++/Problem 14.cpp
@@ -1,53 +1,32 @@
 #include <iostream>
 #include <vector>
-//#include <limits>
 
-bool teilbar_durch_2(auto zahl){
-    if((zahl > 1) && (zahl % 2 == 0)){
-        return true;
-    }
-    return false;
-}
+bool ist_gerade(std::size_t num) { return num % 2 == 0; }
 
-auto durch_2_teilen(auto zahl){
-    return zahl/2;
-}
-
-auto mal_3_plus_1(auto zahl){
-    return zahl*3+1;
-}
 //1'000'000
-int main(){
-    std::vector<unsigned int> max_terme_zahlen;
-    unsigned int terme{1};
-    unsigned int zahl{1};
-    unsigned int max_terme{0};
-    for(; zahl <= 1'000'000; zahl++,terme=0){
-        std::vector<unsigned int> tmp;
-        unsigned int tmp_zahl=zahl;
-        for(;tmp_zahl > 1;terme++){
-            tmp.push_back(tmp_zahl);
-            if(teilbar_durch_2(tmp_zahl)){
-                tmp_zahl = durch_2_teilen(tmp_zahl);
-            }
-            else{
-                tmp_zahl = mal_3_plus_1(tmp_zahl);
-            }
-        }tmp.push_back(tmp_zahl);
-        if(terme > max_terme){
-            max_terme = terme;
-            max_terme_zahlen.clear();
-            max_terme_zahlen = tmp;
-        }
-        tmp.clear();
-    }
-    for(unsigned int z{0}; z<max_terme_zahlen.size(); z++){
-        std::cout << max_terme_zahlen[z];
-        if(z+1 < max_terme_zahlen.size()){
-            std::cout << ' ';
-        }
-    }
-    std::cout << "\n\tDiese Operation benoetigte " << max_terme << " Terme\n";
+int main() {
+	unsigned int numWithMaxTerms = 0;
+	unsigned short maxTerms = 0;
+	for(unsigned int i = 0; i < 1'000'000; ++i) {
+		std::size_t tmpNum = i;
+		std::size_t terms = 0;
+		while (tmpNum > 1) {
+			if(ist_gerade(tmpNum) == true) {
+				tmpNum /= 2;
+			} else {
+				tmpNum *= 3;
+				tmpNum += 1;
+			}
+			++terms;
+		}
+		if(terms > maxTerms) {
+			maxTerms = terms;
+			numWithMaxTerms = i;
+		}
+	}
 
-    return 0;
+	std::cout << "numWithMaxTerms: " << numWithMaxTerms << '\n';
+	std::cout << "maxTerms: " << maxTerms << '\n';
+
+	return 0;
 }
