e28c2ccfb3e08c540a1c556db9d49f1b6c10f17a
Jair <Github Sketch Code@users.noreply.github.com>
2021-05-14 16:46:22 +0300

    Optimierung

--- C++/Problem 23.cpp
+++ C++/Problem 23.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 #include <list>
 
-// Slow and requires a lot of memory(over 1G) -- Need to optimize it
+#define UPPER_LIMIT 28123 // The given "upper limit". All greater numbers can be written as sum of two abundant numbers.
+
+// !!! ----- requires a lot of memory(over 700MB) ----- !!!
 
 bool isProperDivisor(std::size_t num, std::size_t divisor) {
 	return num % divisor == 0;
@@ -32,7 +34,7 @@ bool isDeficientNumber(std::size_t num) {
 
 int main() {
 	std::size_t sum_of_integers = 0; // Sum of all integers from 1 to 28123
-	for(unsigned short i = 0; i <= 28123; i++) {
+	for(unsigned short i = 0; i <= UPPER_LIMIT; i++) {
 		sum_of_integers += i;
 	}
 	std::cout << "Sum of all integers(1-28123): " << sum_of_integers << '\n';
@@ -40,7 +42,7 @@ int main() {
 	std::vector<std::size_t> abundant_numbers;
 
 	// Calculate all abundant numbers under the "given" limit(28123)
-	for(std::size_t i = 1; i <= 28123; i++) {
+	for(std::size_t i = 1; i <= UPPER_LIMIT; i++) {
 		if(isAbundantNumber(i) == true) {
 			abundant_numbers.push_back(i);
 		}
@@ -49,9 +51,14 @@ int main() {
 	// All Numbers which can be written as sum of two abundant numbers(from 1 to 28123)
 	std::list<std::size_t> sum_of_two_abundant_numbers;
 
-	for(auto num1 : abundant_numbers) {
-		for(auto num2 : abundant_numbers) {
-			sum_of_two_abundant_numbers.insert(sum_of_two_abundant_numbers.end(), num1 + num2);
+	// 2-te for schleif "j = i" -->
+	// Man muss nicht nochmal durch alle elemente durchlaufen, man hat sie ja schon in vorigen durchlaeufen behandelt - 
+	// z.B. am anfang 1 - 28123, dann 2 - 28123 am ende nur noch 28122 - 28123
+	// bei i = 28122 hat man ja die vorigen nummern schon behandelt - es fehlen nur noch dir nummern j > i && j <= UPPER_LIMIT
+	// ----> Grosse Performance-Verbesserung, da viel weniger Durchlaeufe gemacht werden muessen!!
+	for(unsigned short i = 0; i < abundant_numbers.size(); ++i) {
+		for(unsigned short j = i; j < abundant_numbers.size(); ++j) {
+			sum_of_two_abundant_numbers.insert(sum_of_two_abundant_numbers.end(), abundant_numbers[i] + abundant_numbers[j]);
 		}
 	}
 
@@ -60,7 +67,7 @@ int main() {
 
 	// Subtract the numbers which can be written as sum of two abundant numbers from the sum of all numbers form 1 to 29123
 	for(auto it = sum_of_two_abundant_numbers.begin(); it != sum_of_two_abundant_numbers.end(); ++it) {
-		if(*it <= 28123) {
+		if(*it <= UPPER_LIMIT) {
 			sum_of_integers -= *it;
 		}
 	}	
